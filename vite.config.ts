48633cfd07a3b5e302820a54d1b41fda8f9e4991
diffurchik <de.Aliatta@gmail.com>
2024-12-25 21:10:51 +0100

    main change deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: ' https://diffurchik.github.io/profile/',
   build: {
     rollupOptions: {
       // https://rollupjs.org/configuration-options/
